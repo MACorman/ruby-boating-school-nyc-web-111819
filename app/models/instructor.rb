369cf27fc94298871453852eb6db13943563c291
MACorman <madelinecorman@gmail.com>
2019-11-25 11:16:16 -0500

    completed boating school lab

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,48 @@
 class Instructor
 
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def boating_test
+        BoatingTest.all.select {|bt| bt.instructor == self}
+    end
+
+    def find_test(student, test_name)
+        boating_test.select do |bt| 
+            bt.student == student && bt.test_name == test_name
+       end
+    end
+
+    def pass_student(student, test_name)
+         test_taken = find_test(student, test_name)
+         if test_taken == [] 
+            BoatingTest.new(student, test_name, "passed", self)
+         else
+            test_taken[0].test_status = "passed"
+            test_taken
+            
+         end 
+    end
+
+    def fail_student(student, test_name)
+        test_taken = find_test(student, test_name)
+         if test_taken == [] 
+            BoatingTest.new(student, test_name, "failed", self)
+         else
+            test_taken[0].test_status = "failed"
+            test_taken
+            
+         end 
+    end
+
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,29 @@
 class Student
 
+    attr_reader :first_name
+
+    @@all = []
+
+    def initialize(first_name)
+        @first_name = first_name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def add_boating_test(test_name, test_status, instructor)
+        BoatingTest.new(self, test_name, test_status, instructor)
+    end
+
+    def self.find_student(first_name)
+        @@all.select {|stu| stu.first_name == first_name}
+    end
+
+    def grade_percentage
+        new_array = BoatingTest.all.select {|bt| bt.student == self}
+        (new_array.select {|bt| bt.test_status == "passed"}).length.to_f / new_array.length
+    end
+
 end
